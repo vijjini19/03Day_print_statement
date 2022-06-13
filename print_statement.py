05f8254e64259ca7fa48296fd07639c0109b96e7
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-13 14:32:16 +0530

    Add files via upload

---
+++ print_statement.py
@@ -0,0 +1,77 @@
+# print('python programming')
+# print("python program")
+
+""" Error codes for line 5 & 6 """
+# print('python program'ming')
+# print("python progr"am")
+
+# print('python program"ming')
+# print("python prog'gram")
+
+# print("python program'ming")
+# print('python prog"ram')
+
+# print('python program\'ming')
+# print("python prog\"am")
+
+# print('we are learning \
+# python programming')
+
+# print("we are learning \
+# python programming")
+
+# print('''we are learning
+# python programming''')
+
+# print("""we are learning 
+# pyhton programming""")
+
+# print('''we are learn'ing py"thon programming''')
+# print("""we are learn'ing py"thon program""")
+
+# print('''i'm doing python programming''')
+# print('''i\'m doing python programming''')
+
+# print('hello')
+# print()
+# print('ok take care')
+
+# print(type(''))
+# print(type(""))
+# print(type(''''''))
+# print(type(""""""))
+
+# print('hello \
+# \
+# python')
+
+# print('''hello
+
+# python''')    
+
+"""to print Hello World in all possible ways"""
+# print('Hello World')
+# print("Hello World")
+# print('''Hello World''')
+# print("""Hello World""")
+
+"""to print Hello Wor'ld in all possible ways"""
+# print("Hello Wor'ld")
+# print('Hello Wor\'ld')
+# print("""Hello Wor'ld""")
+# print('''Hello Wor'ld''')
+
+"""to print print(Hello World) in all possible ways"""
+# print("print(Hello World)")
+# print('print(Hello World)')
+# print("""print(Hello World)""")
+# print('''print(Hello World)''')
+
+"""a string can be multiplies only with integer(int)"""
+# print('hello '*5)
+# print(3*'hello')
+
+"""a string cannot be multiplied with str and float"""
+# print('hey'*'bye')
+# print('hey'*3.6)
+
